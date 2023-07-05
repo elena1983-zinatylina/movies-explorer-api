f8a28b8facf64ce740bd880d711b75753cad29c4
elena1983-zinatylina <tortdekor2016@yandex.ru>
2023-07-05 14:00:36 +0300

    папка utils

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const { errors } = require('celebrate');
-const cors = require('cors');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
-const userRouter = require('./routes/users');
-const cardRouter = require('./routes/cards');
-const auth = require('./middlewares/auth');
+const { errors } = require('celebrate');
+const errorHandler = require('./middlewares/errorHandler');
+const router = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { MONGO_URL } = require('./utils/constants');
+const limiter = require('./middlewares/requestsLimiter');
 
-const { createUser, login } = require('./controllers/users');
-const { signinValidator, signupValidator } = require('./middlewares/validation');
-const NotFoundError = require('./utils/errors/NotFoundError');
-
-const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 app.use(cors());
@@ -24,51 +22,22 @@ mongoose.connect(MONGO_URL)
 
 mongoose.set({ runValidators: true });
 
-// подключаю парсеры
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(limiter);
+
+app.use(helmet());
 
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-// роуты  пользователя
-app.post('/signin', signinValidator, login);
-app.post('/signup', signupValidator, createUser);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Защита авторизацией
-app.use(auth);
-
-// подключаю роутинг
-app.use('/', userRouter);
-app.use('/', cardRouter);
-
-app.all('/*', (req, res, next) => {
-  next(new NotFoundError('Страница не существует'));
-});
+app.use(router);
 
 app.use(errorLogger);
 
-// обработчики ошибок
 app.use(errors());
-app.use((err, req, res, next) => {
-  const {
-    statusCode = 500,
-    message,
-  } = err;
-  res.status(statusCode)
-    .send({
-      message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
-      err: err.message,
-      stack: err.stack,
-    });
-  next();
-});
+app.use(errorHandler);
 
-// Если всё работает, консоль покажет, какой порт приложение слушает
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
