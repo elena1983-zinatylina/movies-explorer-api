d704d883f007d4af7673cdbe946ed0eb945e4586
elena1983-zinatylina <tortdekor2016@yandex.ru>
2023-07-05 14:22:12 +0300

    исправляю models user

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const { isURL, isEmail } = require('validator');
 const bcrypt = require('bcryptjs');
+const { isEmail } = require('validator');
 const UnauthorizedError = require('../utils/errors/UnauthorizedError');
 
 const userSchema = new mongoose.Schema({
@@ -8,19 +8,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: 2,
     maxlength: 30,
-    default: 'Жак-Ив Кусто',
-  },
-  about: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    default: 'Исследователь',
-  },
-  avatar: {
-    type: String,
-    default:
-      'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-    validate: [isURL],
   },
   email: {
     type: String,
