6107a8bd7aa4b7accaca7ff01a49b3c64bf05ac5
elena1983-zinatylina <tortdekor2016@yandex.ru>
2023-07-06 08:41:45 +0300

    ИЗМЕНИЛА ПОРТ 3001

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { MONGO_URL } = require('./utils/constants');
 const { limiter } = require('./middlewares/rateLimiter');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 const app = express();
 app.use(cors());
